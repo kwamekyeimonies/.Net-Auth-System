8c54f48ba04777cd4682a20349848877a239de35
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-05-10 03:22:39 +0000

    basic structure

---
+++ AuthSystem/Program_example.cs
@@ -0,0 +1,24 @@
+// var builder = WebApplication.CreateBuilder(args);
+// var app = builder.Build();
+// // Add services to the container.
+
+// builder.Services.AddControllers();
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+
+
+
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+// // app.UseHttpsRedirection();
+
+// // app.UseAuthorization();
+
+// // app.MapControllers();
+
+// app.Run();
