828e0d8f39d60f6bc36bb77545323c4825adb321
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-05-11 04:36:45 +0000

    3 level authentication

--- AuthSystem/Program.cs
+++ AuthSystem/Program.cs
@@ -6,9 +6,45 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication().AddCookie("local").AddCookie("visitor");
+builder.Services.AddAuthentication()
+.AddScheme<CookieAuthenticationOptions, VisitorAuthHandler>("visitor", o => { })
+.AddCookie("local")
+.AddCookie("patreon-cookie")
+.AddOAuth(
+    "external-patreon",
+    o =>
+    {
+        o.SignInScheme = "patreon-cookie";
 
-builder.Services.AddAuthorization();
+        o.ClientId = "id";
+        o.ClientSecret = "secret";
+        o.AuthorizationEndpoint = "https://oauth.mocklab.io/oauth/authorize";
+        o.TokenEndpoint = "https://oauth.mocklab.io/oauth/token";
+        o.UserInformationEndpoint = "https://oauth.mocklab.io/userinfo";
+
+        o.CallbackPath = "/cb-patreon";
+
+        o.Scope.Add("profile");
+        o.SaveTokens = true;
+    }
+);
+
+builder.Services.AddAuthorization(
+    b =>
+    {
+        b.AddPolicy(
+            "customer",
+            p =>
+            {
+                p.AddAuthenticationSchemes("local", "visitor").RequireAuthenticatedUser();
+            });
+             b.AddPolicy(
+            "user",
+            p =>
+            {
+                p.AddAuthenticationSchemes("local", "visitor").RequireAuthenticatedUser();
+            });
+    });
 
 var app = builder.Build();
 
@@ -18,7 +54,7 @@ app.UseAuthorization();
 app.MapGet(
     "/",
     ctx => Task.FromResult("Auth System development")
-);
+).RequireAuthorization("customer");
 
 app.MapGet(
     "/login-local",
@@ -26,23 +62,54 @@ app.MapGet(
     {
         var claims = new List<Claim>();
         claims.Add(new Claim("usr", "DanielTenkorang"));
-        var identity = new ClaimsIdentity(claims, "cookie");
+        var identity = new ClaimsIdentity(claims, "local");
         var user = new ClaimsPrincipal(identity);
 
         await ctx.SignInAsync("local", user);
     }
 );
 
+
+app.MapGet(
+    "/login-patreon",
+    async (ctx) =>
+    {
+        await ctx.ChallengeAsync("external-patreon",new AuthenticationProperties(){
+            RedirectUri = "/"
+        });
+    }
+).RequireAuthorization("user");
+
 app.Run();
 
 public class VisitorAuthHandler : CookieAuthenticationHandler
 {
     public VisitorAuthHandler(
-        IOptionsMonitor<CookieAuthenticationOptions> options, 
+        IOptionsMonitor<CookieAuthenticationOptions> options,
         ILoggerFactory logger,
         UrlEncoder encoder,
         ISystemClock clock
-        ) : base(options,logger,encoder,clock){
-            
+        ) : base(options, logger, encoder, clock)
+    {
+
+    }
+
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var result = await base.HandleAuthenticateAsync();
+
+        if (result.Succeeded)
+        {
+            return result;
         }
+
+        var claims = new List<Claim>();
+        claims.Add(new Claim("usr", "DanielTenkorang"));
+        var identity = new ClaimsIdentity(claims, "visitor");
+        var user = new ClaimsPrincipal(identity);
+
+        await Context.SignInAsync("visitor", user);
+
+        return AuthenticateResult.Success(new AuthenticationTicket(user, "visitor"));
+    }
 }
