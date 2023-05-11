47d6a42ad3caa74e36c9b415799eeb9952f353d1
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-05-11 02:08:02 +0000

    auth schema dev

--- AuthSystem/Program.cs
+++ AuthSystem/Program.cs
@@ -1,76 +1,48 @@
-using Microsoft.AspNetCore.DataProtection;
-using AuthSystem.Service;
 using System.Security.Claims;
+using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
-//Adding Data Protection Layer for Encryption and decrypting of Data
-// builder.Services.AddDataProtection();
-// builder.Services.AddScoped<AuthService>();
-// builder.Services.AddHttpContextAccessor();
 
-builder.Services.AddAuthentication("cookie");
+builder.Services.AddAuthentication().AddCookie("local").AddCookie("visitor");
 
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// app.Use((ctx, next) =>
-// {
-//     var idp = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>();
-//     var protector = idp.CreateProtector("auth-cookie");
-//     var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
-//     if (authCookie != null)
-//     {
-//         var protectedPayload = authCookie.Split("=").Last();
-
-//         var payload = protector.Unprotect(protectedPayload);
-//         var parts = payload.Split(":");
-//         var key = parts[0];
-//         var value = parts[1];
-
-//         var claims = new List<Claim>();
-//         claims.Add(new Claim(key, value));
-//         var identity = new ClaimsIdentity(claims);
-//         ctx.User = new ClaimsPrincipal();
-
-//     }
-//     return next();
-// }
-// );
-
 app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGet(
-    "/username", (HttpContext ctx) =>
-    {
-        // return ctx.User.FindFirst("usr").Value;
-        var user = ctx.User;
-        if (user != null)
-        {
-            var claim = user.FindFirst("user");
-            if (claim != null)
-            {
-                return claim.Value;
-            }
-        }
-
-        return "No user claim found";
-    }
+    "/",
+    ctx => Task.FromResult("Auth System development")
 );
 
 app.MapGet(
-    "/login", async(HttpContext ctx) =>
+    "/login-local",
+    async (ctx) =>
     {
-        // auth.SignIn();
         var claims = new List<Claim>();
-        claims.Add(new Claim("usr","DanielTenkorang"));
-        var identity = new ClaimsIdentity(claims,"cookie");
-        var user = new ClaimsPrincipal();
+        claims.Add(new Claim("usr", "DanielTenkorang"));
+        var identity = new ClaimsIdentity(claims, "cookie");
+        var user = new ClaimsPrincipal(identity);
 
-        await ctx.SignInAsync("cookie",user);
-        return "OK";
+        await ctx.SignInAsync("local", user);
     }
 );
-
 
 app.Run();
+
+public class VisitorAuthHandler : CookieAuthenticationHandler
+{
+    public VisitorAuthHandler(
+        IOptionsMonitor<CookieAuthenticationOptions> options, 
+        ILoggerFactory logger,
+        UrlEncoder encoder,
+        ISystemClock clock
+        ) : base(options,logger,encoder,clock){
+            
+        }
+}
