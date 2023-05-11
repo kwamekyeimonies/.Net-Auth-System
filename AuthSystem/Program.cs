691f0855c44f00b3fa7fdd73e5718a82a445afb9
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-05-11 10:51:32 +0000

    auth and policy building

--- AuthSystem/Program.cs
+++ AuthSystem/Program.cs
@@ -1,115 +1,144 @@
 using System.Security.Claims;
-using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authorization;
+
+const string AuthScheme = "cookie";
+const string AuthScheme2 = "cookie2";
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication()
-.AddScheme<CookieAuthenticationOptions, VisitorAuthHandler>("visitor", o => { })
-.AddCookie("local")
-.AddCookie("patreon-cookie")
-.AddOAuth(
-    "external-patreon",
-    o =>
+builder.Services.AddAuthentication(AuthScheme)
+.AddCookie(AuthScheme)
+.AddCookie(AuthScheme2)
+;
+
+//Configuration Layer for building Policies
+builder.Services.AddAuthorization(
+    (builder) =>
     {
-        o.SignInScheme = "patreon-cookie";
-
-        o.ClientId = "id";
-        o.ClientSecret = "secret";
-        o.AuthorizationEndpoint = "https://oauth.mocklab.io/oauth/authorize";
-        o.TokenEndpoint = "https://oauth.mocklab.io/oauth/token";
-        o.UserInformationEndpoint = "https://oauth.mocklab.io/userinfo";
-
-        o.CallbackPath = "/cb-patreon";
-
-        o.Scope.Add("profile");
-        o.SaveTokens = true;
+        builder.AddPolicy("ecowas passport",
+        (pb) =>
+        {
+            pb.RequireAuthenticatedUser()
+            .AddAuthenticationSchemes(AuthScheme)
+            .AddRequirements()
+            .RequireClaim("passport_type", "ecowas");
+        });
     }
 );
-
-builder.Services.AddAuthorization(
-    b =>
-    {
-        b.AddPolicy(
-            "customer",
-            p =>
-            {
-                p.AddAuthenticationSchemes("local", "visitor").RequireAuthenticatedUser();
-            });
-             b.AddPolicy(
-            "user",
-            p =>
-            {
-                p.AddAuthenticationSchemes("local", "visitor").RequireAuthenticatedUser();
-            });
-    });
 
 var app = builder.Build();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapGet(
-    "/",
-    ctx => Task.FromResult("Auth System development")
-).RequireAuthorization("customer");
+// app.Use((ctx, next) =>
+// {
+
+//     if (ctx.Request.Path.StartsWithSegments("/login"))
+//     {
+//         return next();
+//     }
+
+//     if (!ctx.User.Identities.Any(x => x.AuthenticationType == AuthScheme))
+//     {
+//         ctx.Response.StatusCode = 401;
+
+//         return Task.CompletedTask;
+//     }
+
+//     if (!ctx.User.HasClaim("passport_type", "ecowas"))
+//     {
+//         ctx.Response.StatusCode = 403;
+
+//         return Task.CompletedTask;
+//     }
+
+//     return next();
+// });
 
 app.MapGet(
-    "/login-local",
-    async (ctx) =>
+    "/unsecure",
+    (HttpContext ctx) =>
     {
-        var claims = new List<Claim>();
-        claims.Add(new Claim("usr", "DanielTenkorang"));
-        var identity = new ClaimsIdentity(claims, "local");
-        var user = new ClaimsPrincipal(identity);
-
-        await ctx.SignInAsync("local", user);
+        return ctx.User.FindFirst("usr")?.Value ?? "empty";
     }
 );
 
 
+// Using controllers you can specify your policy by adding
+// [Authorize(Policy="ecowas passport")]
 app.MapGet(
-    "/login-patreon",
-    async (ctx) =>
+    "/ghana",
+    (HttpContext ctx) =>
     {
-        await ctx.ChallengeAsync("external-patreon",new AuthenticationProperties(){
-            RedirectUri = "/"
-        });
+        // if (!ctx.User.Identities.Any(x => x.AuthenticationType == AuthScheme))
+        // {
+        //     ctx.Response.StatusCode = 401;
+
+        //     return "Not Authenticated(401)";
+        // }
+
+        // if (!ctx.User.HasClaim("passport_type", "ecowas"))
+        // {
+        //     ctx.Response.StatusCode = 403;
+        //     return "Forbidden Response Status Code(403)";
+        // }
+
+        return "Passport allowed && Certified";
     }
-).RequireAuthorization("user");
+).RequireAuthorization("ecowas passport");
+
+// There is redirect built into the authorization middleware
+
+app.MapGet(
+    "/nigeria",
+    (HttpContext ctx) =>
+    {
+        // if (!ctx.User.Identities.Any(x => x.AuthenticationType == AuthScheme2))
+        // {
+        //     ctx.Response.StatusCode = 401;
+
+        //     return "Not Authenticated(401)";
+        // }
+
+        // if (!ctx.User.HasClaim("passport_type", "ecowas"))
+        // {
+        //     ctx.Response.StatusCode = 403;
+        //     return "Forbidden Response Status Code(403)";
+        // }
+
+        return "Passport allowed && Certified";
+    }
+).RequireAuthorization("ecowas passport");
+
+app.MapGet(
+    "/login",
+    async (HttpContext ctx) =>
+    {
+        var claims = new List<Claim>();
+        claims.Add(new Claim("usr", "Daniel Tenkorang"));
+        claims.Add(new Claim("passport_type", "ecowas"));
+        var identity = new ClaimsIdentity(claims, AuthScheme);
+        var user = new ClaimsPrincipal(identity);
+
+        await ctx.SignInAsync(AuthScheme, user);
+    }
+).AllowAnonymous();
+
+// To allow login to reach unauthenticated users we add AllowAnonymous();
 
 app.Run();
 
-public class VisitorAuthHandler : CookieAuthenticationHandler
+
+public class MyAuthRequirements : IAuthorizationRequirement { }
+
+public class MyAuthRequirementsHandler : AuthorizationHandler<MyAuthRequirements>
 {
-    public VisitorAuthHandler(
-        IOptionsMonitor<CookieAuthenticationOptions> options,
-        ILoggerFactory logger,
-        UrlEncoder encoder,
-        ISystemClock clock
-        ) : base(options, logger, encoder, clock)
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, MyAuthRequirements requirement)
     {
+        // context.Succeed(MyAuthRequirements());
 
-    }
-
-    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
-    {
-        var result = await base.HandleAuthenticateAsync();
-
-        if (result.Succeeded)
-        {
-            return result;
-        }
-
-        var claims = new List<Claim>();
-        claims.Add(new Claim("usr", "DanielTenkorang"));
-        var identity = new ClaimsIdentity(claims, "visitor");
-        var user = new ClaimsPrincipal(identity);
-
-        await Context.SignInAsync("visitor", user);
-
-        return AuthenticateResult.Success(new AuthenticationTicket(user, "visitor"));
+        return Task.CompletedTask;
     }
 }
---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+Authentication && Authorization 
+
+Authentication is the process of validating user creditials whilst authorization is the process of checking priveleges for a user to access a specific modules in an application.
+Authentication --> Knowing the identify of the user
+Authorization --> Deciding whether a user is allowed to perform an action.
+
